501632fa6758f233e7f8b762368c3282986a04d0
Parth Lotte <parth33998@gmail.com>
2022-12-19 01:48:00 +0530

    4c

---
+++ Characterpatter.java
@@ -0,0 +1,16 @@
+public class Characterpatter {
+    public static void main (String args[]){
+        int n=4;
+        char ch='A';
+        for(int line=1; line<=n;line++)
+        {
+            for (int j=1;j<=line;j++)
+            {
+                System.out.print(ch);
+                ch++;
+            }
+            System.out.println();
+        }
+    }
+}
+
---
+++ halfPyramid.java
@@ -0,0 +1,13 @@
+public class halfPyramid {
+    public static void main (String args[]){
+        int n=4;
+        for(int line=1; line<=n;line++)
+        {
+            for (int j=1;j<=line;j++)
+            {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}
