8be08850b4ccaec7934955a0ee6576d12bef5bf3
Parth Lotte <parth33998@gmail.com>
2023-01-11 15:52:16 +0530

    Recursion

---
+++ Recursion/factorialRecursion.java
@@ -0,0 +1,17 @@
+public class factorialRecursion {
+
+    public static int  fact(int n)
+    {
+        if(n==0)
+        {
+            return 1;
+        }
+        return n * fact(n-1);
+    }
+   public static void main(String[] args)
+    {
+        int n=3;
+        System.out.println(fact(n));
+        // fact(n);
+    }
+}
