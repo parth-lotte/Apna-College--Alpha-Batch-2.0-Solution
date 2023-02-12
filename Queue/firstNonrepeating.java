e3b5a798aa062c89c1f489f3139a5292fc58891b
Parth Lotte <parth33998@gmail.com>
2023-02-12 12:05:16 +0530

    Non repeating characters

---
+++ Queue/firstNonrepeating.java
@@ -0,0 +1,39 @@
+import java.util.Queue;
+import java.util.LinkedList;
+public class firstNonrepeating {
+
+    public static void nonRepeating(String str)
+    {
+     int freq[]= new int[26]; // 'a' - 'z'
+Queue<Character> q= new LinkedList<>();
+
+        for(int i=0;i<str.length();i++)
+        {
+            char ch= str.charAt(i);
+            q.add(ch);
+            
+            freq[ch-'a']++; // checking for the frequency 
+
+            while(!q.isEmpty() && freq[q.peek()-'a']>1)
+            {
+                q.remove();
+            }
+            if(q.isEmpty())
+            {
+                System.out.println(-1+ " ");
+            }
+            else
+            {
+                System.out.println(q.peek()+ " ");
+            }
+        }
+        System.out.println();
+    }
+    public static void main(String args[])
+    {
+        String str="aabccxb";
+        nonRepeating(str);
+        
+    }
+    
+}
