4758f726b58972530047f52680c3ec01856e4695
Parth Lotte <parth33998@gmail.com>
2023-03-08 12:57:18 +0530

    Top View Of Tree

--- Binary Trees/diameterTree.java
+++ Binary Trees/diameterTree.java
@@ -1,4 +1,7 @@
 import java.util.*;
+import java.util.Queue;
+
+import javax.sound.sampled.DataLine.Info;
 public class diameterTree {
 
     static class Node{
@@ -82,6 +85,75 @@ public class diameterTree {
         }
             return isSubtree(root.left,subRoot) || isSubtree(root.right,subRoot); 
      }
+
+
+
+     // TOP VIEW OF A TREE
+
+
+     static class Info{
+        Node node;
+        int hd;
+
+        public Info(Node node, int hd)
+        {
+            this.node= node;
+            this.hd= hd;
+        }
+     }
+
+     public static void topView (Node root)
+     {
+        // Level Order
+
+        Queue<Info> q= new LinkedList<>();
+        HashMap<Integer,Node> map= new HashMap<>();
+
+        int min=0, max=0;
+        q.add(new Info(root,0));
+        q.add(null);
+
+        while(!q.isEmpty())
+        {
+            Info curr= q.remove();
+            if(curr== null)
+            {
+                if(q.isEmpty())
+                {
+                    break;
+                }
+                else {
+                    q.add(null);
+                }
+            }else 
+            {
+                if(!map.containsKey(curr.hd))
+            {
+                map.put(curr.hd,curr.node);
+            }
+            if(curr.node.left!=null)
+            {
+                q.add(new Info(curr.node.left, curr.hd-1));
+                min=Math.min(min,curr.hd-1);
+            }
+            if(curr.node.right!=null)
+            {
+                q.add(new Info(curr.node.right, curr.hd+1));
+                min=Math.min(min,curr.hd+1);
+            }
+            }
+
+            
+        }
+        for(int i= min; i<=max;i++)
+        {
+            System.out.println(map.get(i).data+" ");
+        }
+        System.out.println();
+    
+    }
+
+
     public static void main(String args[])
     {
         Node root = new Node(1);
@@ -91,18 +163,19 @@ public class diameterTree {
         root.left.right = new Node(5);
         root.right.left= new Node (6);
         root.right.right= new Node(7);
-        System.out.println(height(root));
+        // System.out.println(height(root));
         System.out.println();
 
-        System.out.println(diameter(root));
+        // System.out.println(diameter(root));
 
+            topView(root);
         // subtreee
 
-        Node subRoot= new Node(2);
-        subRoot.left= new Node(4);
-        subRoot.right= new Node(5);
+        // Node subRoot= new Node(2);
+        // subRoot.left= new Node(4);
+        // subRoot.right= new Node(5);
 
-        System.out.println(isSubtree(root, subRoot));
+        // System.out.println(isSubtree(root, subRoot));
     }
     
 }
