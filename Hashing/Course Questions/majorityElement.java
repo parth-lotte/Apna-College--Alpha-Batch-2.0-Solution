d5296ec7722a0e3743f1fbed9b04bc5b93d7e981
Parth Lotte <parth33998@gmail.com>
2023-04-15 15:22:46 +0530

    Majority Element

---
+++ Hashing/Course Questions/majorityElement.java
@@ -0,0 +1,30 @@
+import java.util.HashMap;
+import java.util.*;
+// package Course Questions;
+
+public class majorityElement {
+    public static void main(String args[])
+    {
+        int arr[]= {1,3,2,5,1,3,1,5,1};
+        HashMap<Integer,Integer> hm= new HashMap<>();
+        for(int i=0;i<arr.length;i++)
+        {
+            if(hm.containsKey(arr[i]))
+            {
+                hm.put(arr[i],hm.get(arr[i])+1);
+            }
+            else {
+                hm.put(arr[i],1);
+            }
+        }
+
+        Set<Integer> keySet= hm.keySet();
+        for(Integer key : keySet)
+        {
+            if(hm.get(key)> arr.length/3)
+            {
+                System.out.println(key);
+            }
+        }
+    }
+}
