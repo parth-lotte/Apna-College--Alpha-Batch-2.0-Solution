0db88723511099b7a6d08186deec77513ae813f5
Parth Lotte <parth33998@gmail.com>
2023-04-15 14:41:30 +0530

    TreeMap

--- Hashing/Linkedhash.java
+++ Hashing/Linkedhash.java
@@ -3,10 +3,11 @@ import java.util.*;
 public class Linkedhash{
     public static void main(String args[])
     {
-        LinkedHashMap<String,Integer> lhm= new LinkedHashMap<>();
+        TreeMap<String,Integer> lhm= new TreeMap<>();
         lhm.put("India",100);
         lhm.put("China",1020);
         lhm.put("USA",10340);
+        lhm.put("PAK", 9990);
 
         HashMap<String,Integer> hm= new LinkedHashMap<>();
         hm.put("China",1020);
